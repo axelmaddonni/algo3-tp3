27a36f731f1692dfd8dad7a0fa0dd573a7357b1e
gciruelos <gonzalo.ciruelos@gmail.com>
2016-06-02 01:43:14 -0300

    arreglado tema del build

---
+++ src/utilidades.cpp
@@ -0,0 +1,40 @@
+#include "utilidades.h"
+
+std::vector<int> copiar_sin(std::vector<int> v, int x) {
+  std::vector<int> v_copia;
+  for (const auto y : v) {
+    if (y != x) {
+      v_copia.push_back(y);
+    }
+  }
+  return v_copia;
+}
+
+
+bool lin_search(std::vector<int> v, int x) {
+  for (const auto y : v) {
+    if (x == y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int contar_aristas_isomorfismo(Grafo g1, Grafo g2, Isomorfismo iso) {
+  int aristas = 0;
+  for (const auto p : iso) {
+    for (const auto q : iso) {
+      int vg1 = p.first;
+      int vg2 = p.second;
+      int ug1 = q.first;
+      int ug2 = q.second;
+
+      if (g1.adj_matrix[vg1][ug1] && // ug1 y vg1 estan conectados.
+          g2.adj_matrix[vg2][ug2]) { // ug2 y vg2 estan conectados.
+        aristas++;
+      }
+    }
+  }
+
+  return aristas / 2; // conte todas 2 veces;
+}
--- src/utilidades.h
+++ src/utilidades.h
@@ -22,42 +22,11 @@ struct MCS{
   int aristas;
 };
 
-std::vector<int> copiar_sin(std::vector<int> v, int x) {
-  std::vector<int> v_copia;
-  for (const auto y : v) {
-    if (y != x) {
-      v_copia.push_back(y);
-    }
-  }
-  return v_copia;
-}
 
+std::vector<int> copiar_sin(std::vector<int> v, int x);
 
-bool lin_search(std::vector<int> v, int x) {
-  for (const auto y : v) {
-    if (x == y) {
-      return true;
-    }
-  }
-  return false;
-}
+bool lin_search(std::vector<int> v, int x);
 
-int contar_aristas_isomorfismo(Grafo g1, Grafo g2, Isomorfismo iso) {
-  int aristas = 0;
-  for (const auto p : iso) {
-    for (const auto q : iso) {
-      int vg1 = p.first;
-      int vg2 = p.second;
-      int ug1 = q.first;
-      int ug2 = q.second;
+int contar_aristas_isomorfismo(Grafo g1, Grafo g2, Isomorfismo iso);
 
-      if (g1.adj_matrix[vg1][ug1] && // ug1 y vg1 estan conectados.
-          g2.adj_matrix[vg2][ug2]) { // ug2 y vg2 estan conectados.
-        aristas++;
-      }
-    }
-  }
-
-  return aristas / 2; // conte todas 2 veces;
-}
 #endif
