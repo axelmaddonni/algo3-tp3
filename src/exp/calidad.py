cfe11c0cc0cf274f09b19642d3c5b3e5e2c534e9
gciruelos <gonzalo.ciruelos@gmail.com>
2016-06-13 04:15:54 -0300

    calidad.py

--- src/exp/calidad.py
+++ src/exp/calidad.py
@@ -1,26 +1,31 @@
 import sys
+import subprocess
 
 def correr_programa(binario, archivo):
-    p = subprocess.Popen("cat " + archivo + " | " + binario + " | head -n 1",
+    comando = binario + " < "  + archivo
+    p = subprocess.Popen(comando,
                          shell = True,
                          stdin=subprocess.PIPE,
                          stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    p.stdin.write(input_string)
     out, err = p.communicate()
     return int(out.split()[1])
 
 def main():
     size = 50
-    q = [500, 400, 200, 100, 50, 10]
-    ms =[[],  [] , [] , [] , [], []]
-    for i in range(6):
-        random.seed(i)
-        size *= 2
-        for j in range(q[i]):
-            print i, j
-            nombre_archivo = "caso" + str(size) + "_" + str(j) + ".txt"
-            nombre_archivo = "casos_random/casos_ahora/" + nombre_archivo
-            ms[i].append(correr_programa(binario = sys.argv[1]))
+    casos = [(50, 500), (100, 500), (150, 400), (200, 400),
+             (250, 300), (300, 300), (350, 200), (400, 200),
+             (450, 100), (500, 100)]
+    ms = []
+    for tam, corridas in casos:
+        lista = []
+        for i in range(corridas):
+            print tam, corridas
+            nombre_archivo = "caso" + str(tam) + "_" + str(i) + ".txt"
+            nombre_archivo = "exp/casos_random/casos_ahora/" + nombre_archivo
+            aristas = correr_programa(sys.argv[1], nombre_archivo)
+            print aristas
+            lista.append(aristas)
+        ms.append(lista)
     print ms
 
 
